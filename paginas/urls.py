5f41873e20626c8a3c2ade86c4f65f357f09d19b
Fabricio Dias De Lima <fabricio.dev007@gmail.com>
2024-08-09 15:28:12 -0300

    criado pagina Sobre/Contatos e linkado elas

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexView
-
+from .views import IndexView, SobreView,  ContatoView
 
 urlpatterns = [
-    path('inicio/',IndexView.as_view(), name='paginas-index'),
+    path('inicio/', IndexView.as_view(), name='paginas-index'),
+    path('sobre/', SobreView.as_view(), name='paginas-sobre'),
+    path('contato/', ContatoView.as_view(), name='paginas-contato'),
     
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -2,7 +2,12 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 
+class SobreView(TemplateView):
+    template_name = "sobre.html"
+    
 class IndexView(TemplateView):
     template_name = "index.html"
     
+class ContatoView(TemplateView):
+    template_name = "contato.html"
 
